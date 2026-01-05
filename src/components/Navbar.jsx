5869763a73cc52940b6fda7bab2ccc331b83c21f
Henrik Christoffer Kran <henrik.christoffer.kran@gmail.com>
2026-01-05 19:10:03 +0100

    Home ferdig

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,28 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom"; // Importer useLocation
 import homeLogo from "../assets/Home.jpg";
 import "./Navbar.css";
 
 export function Navbar() {
+  const location = useLocation(); // Hent nåværende sti
+
+  // Sjekk om vi er på hjemmesiden
+  const isHomePage = location.pathname === "/";
+
   return (
     <nav className="navbar-wrapper">
-      {/* Denne diven vil nå skjules på mobil via CSS */}
+      {/* Profilbildet vises bare hvis vi IKKE er på hjemmesiden */}
       <div className="logo-container">
-        <NavLink to="/">
-          <img src={homeLogo} alt="Hjem" className="nav-logo-img" />
-        </NavLink>
+        {!isHomePage && (
+          <NavLink to="/">
+            <img src={homeLogo} alt="Hjem" className="nav-logo-img" />
+          </NavLink>
+        )}
       </div>
 
       <div className="navbar-pill">
         <NavLink to="/About" className="nav-item">Om</NavLink>
         <NavLink to="/Experience" className="nav-item">Erfaring</NavLink>
-        <NavLink to="/SportsCareer" className="nav-item">Idretts karriere</NavLink>
+        <NavLink to="/SportsCareer" className="nav-item">Idrett</NavLink>
         <NavLink to="/Contact" className="nav-item">Kontakt</NavLink>
       </div>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,74 @@
+import CVImg from "../assets/Home.jpg";
+import "./Home.css";
 
+// Oppdaterte importer basert på din filstruktur i VS Code
+import ntnuImg from "../assets/ntnuS.png"; 
+import askoImg from "../assets/askoS.png"; 
+import gsvImg from "../assets/gsvS.png"; 
+import ntnuiImg from "../assets/ntnuiS.png";
 
+export function Home() {
+  return (
+    <div className="home-container">
+      <section className="hero">
+        <div className="hero-text">
+          <h1 className="hero-title">
+            Masterstudent, <br />
+            patruljefører og landslagsutøver.
+          </h1>
 
-export function Home(){
+          <p className="hero-description">
+            Jeg heter Henrik Christoffer Kran og er masterstudent i datateknologi ved NTNU, 
+            med bakgrunn som patruljefører i GSV og landslagsutøver i bryting.
+          </p>
 
-    return(
-        <>
-            <h1>Home</h1>
+          <div className="hero-socials">
+            <a href="https://linkedin.com/in/henrik-christoffer-kran" target="_blank" rel="noreferrer">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/ditt-brukernavn" target="_blank" rel="noreferrer">
+              <i className="fa-brands fa-github"></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="profile-wrapper">
+          <img src={CVImg} alt="Henrik Christoffer Kran" className="hero-img" />
+        </div>
+      </section>
+
+      <section className="companies-section">
+        <div className="divider-container">
+          <span className="divider-text">Organisasjoner jeg har vært en del av</span>
+          <div className="divider-line"></div>
+        </div>
         
-        </>
-
-
-    )
-
-
+        <div className="logo-grid">
+            <div className="company-logo">
+                <a href="https://www.ntnu.no" target="_blank" rel="noreferrer">
+                <img src={ntnuImg} alt="NTNU" />
+                </a>
+            </div>
+            
+            <div className="company-logo">
+                <a href="https://www.forsvaret.no/om-forsvaret/tjenestesteder/hoybuktmoen-gsv" target="_blank" rel="noreferrer">
+                <img src={gsvImg} alt="GSV" />
+                </a>
+            </div>
+            
+            <div className="company-logo">
+                <a href="https://ntnui.no/roing/" target="_blank" rel="noreferrer">
+                <img src={ntnuiImg} alt="NTNUI" />
+                </a>
+            </div>
+            
+            <div className="company-logo">
+                <a href="https://asko.no/kontakt-oss/vare-asko-selskap/asko-oslofjord-as/" target="_blank" rel="noreferrer">
+                <img src={askoImg} alt="ASKO" />
+                </a>
+            </div>
+            </div>
+      </section>
+    </div>
+  );
 }
